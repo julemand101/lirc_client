c3142df25edb318a82b2950c4fba8c7532f4b559
Jacob Bang <julemand101@archlinux.dk>
2025-02-21 08:49:00 +0100

    Dart 3.7 + Formatting + Update example

--- example/lirc_client_example.dart
+++ example/lirc_client_example.dart
@@ -1,18 +1,57 @@
+import 'dart:convert';
+import 'dart:io';
+
 import 'package:lirc_client/lirc_client.dart';
+
+const dac = 'NAD_D1050';
+const receiver = 'NAD_SR6';
 
 Future<void> main() async {
   final client = await LircClient.connect();
 
-  client.broadcastMessages.listen(print);
+  Future<void> turnOn() async {
+    print('### TV POWERED ON ###');
+    client.sendOnce(dac, 'POWER_ON');
+    await Future<void>.delayed(const Duration(seconds: 1));
+    client.sendOnce(receiver, 'KEY_POWER');
+    await Future<void>.delayed(const Duration(seconds: 7));
+    client.sendOnce(dac, 'OPTICAL_1');
+  }
+
+  Future<void> turnOff() async {
+    print('### TV POWERED OFF ###');
+    client.sendOnce(dac, 'POWER_OFF');
+    await Future<void>.delayed(const Duration(seconds: 1));
+    client.sendOnce(receiver, 'off');
+  }
 
   client.broadcastMessages
-      .where((event) => event.remoteControlName == 'Sony_RM-ED009-12')
+      .where((event) => event.remoteControlName == 'sony_rm-ed052')
       .listen((event) => switch (event.buttonName) {
-            'KEY_VOLUMEUP' => client.sendOnce("NAD_SR6", "KEY_VOLUMEUP"),
-            'KEY_VOLUMEDOWN' => client.sendOnce("NAD_SR6", "KEY_VOLUMEDOWN"),
-            'KEY_MUTE' => client.sendOnce("NAD_SR6", "KEY_POWER"),
-            _ => null,
+            'KEY_VOLUMEUP' => client.sendOnce(receiver, 'KEY_VOLUMEUP'),
+            'KEY_VOLUMEDOWN' => client.sendOnce(receiver, 'KEY_VOLUMEDOWN'),
+            'KEY_MUTE' => client.sendOnce(receiver, 'KEY_MUTE'),
+            'KEY_RED' => turnOff(),
+            'KEY_GREEN' => turnOn(),
+            _ => null, //print(value),
           });
 
-  print(":: Program is running!");
+  Process cecProcess = await Process.start('/usr/bin/cec-client', const ['-m']);
+
+  RegExp tvTurningOn =
+      RegExp(r".*TV \(0\): power status changed from '.+' to 'on'");
+
+  cecProcess.stdout
+      .transform(utf8.decoder)
+      .transform(const LineSplitter())
+      .listen((line) async {
+    //print(line);
+    if (line.endsWith('0f:36')) {
+      // TV Power off
+      turnOff();
+    } else if (tvTurningOn.hasMatch(line)) {
+      // TV Power on
+      turnOn();
+    }
+  });
 }
