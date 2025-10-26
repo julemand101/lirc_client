9a9812a064c8e345d9c2267a0446c2eb3c0ca0f2
Jacob Bang <julemand101@archlinux.dk>
2025-10-26 10:57:56 +0100

    Added control of power to DAB radio.

--- example/lirc_client_example.dart
+++ example/lirc_client_example.dart
@@ -28,33 +28,59 @@ Future<void> main() async {
     client.sendOnce(receiver, 'off');
   }
 
+  final httpClient = HttpClient();
+  var lastRadioTrigger = DateTime.timestamp();
+
+  Future<void> setRadioPower(bool on) async {
+    final now = DateTime.timestamp();
+
+    if (now.difference(lastRadioTrigger).inSeconds > 1) {
+      lastRadioTrigger = now;
+      print('### RADIO POWERED ${on ? 'ON' : 'OFF'} ###');
+      final request = await httpClient.getUrl(
+        Uri.parse(
+          'http://192.168.0.109/fsapi/SET/'
+          'netRemote.sys.power?pin=1234&value=${on ? 1 : 0}',
+        ),
+      );
+      final response = await request.close();
+      response.drain<void>().ignore();
+    }
+  }
+
   client.broadcastMessages
       .where((event) => event.remoteControlName == 'sony_rm-ed052')
-      .listen((event) => switch (event.buttonName) {
-            'KEY_VOLUMEUP' => client.sendOnce(receiver, 'KEY_VOLUMEUP'),
-            'KEY_VOLUMEDOWN' => client.sendOnce(receiver, 'KEY_VOLUMEDOWN'),
-            'KEY_MUTE' => client.sendOnce(receiver, 'KEY_MUTE'),
-            'KEY_RED' => turnOff(),
-            'KEY_GREEN' => turnOn(),
-            _ => null, //print(value),
-          });
+      .listen(
+        (event) => switch (event.buttonName) {
+          'KEY_VOLUMEUP' => client.sendOnce(receiver, 'KEY_VOLUMEUP'),
+          'KEY_VOLUMEDOWN' => client.sendOnce(receiver, 'KEY_VOLUMEDOWN'),
+          'KEY_MUTE' => client.sendOnce(receiver, 'KEY_MUTE'),
+          'KEY_RED' => turnOff(),
+          'KEY_GREEN' => turnOn(),
+          'KEY_YELLOW' => setRadioPower(false),
+          'KEY_BLUE' => setRadioPower(true),
+          _ => null, //print(value),
+        },
+      );
 
   Process cecProcess = await Process.start('/usr/bin/cec-client', const ['-m']);
 
-  RegExp tvTurningOn = RegExp(r".*TV \(0\): power status changed from '.+' to "
-      r"'(on)|(in transition from standby to on)'");
+  RegExp tvTurningOn = RegExp(
+    r".*TV \(0\): power status changed from '.+' to "
+    r"'(on)|(in transition from standby to on)'",
+  );
 
   cecProcess.stdout
       .transform(utf8.decoder)
       .transform(const LineSplitter())
       .listen((line) async {
-    //print(line);
-    if (line.endsWith('0f:36')) {
-      // TV Power off
-      turnOff();
-    } else if (tvTurningOn.hasMatch(line)) {
-      // TV Power on
-      turnOn();
-    }
-  });
+        //print(line);
+        if (line.endsWith('0f:36')) {
+          // TV Power off
+          turnOff();
+        } else if (tvTurningOn.hasMatch(line)) {
+          // TV Power on
+          turnOn();
+        }
+      });
 }
